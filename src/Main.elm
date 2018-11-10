0ab021626449f66c33bfc7a9cc00706c9ee73301
Jeremy Greer <jex.grizzle@gmail.com>
2018-11-10 07:22:03 -0500

    Add alternate methods to support Int

--- src/Main.elm
+++ src/Main.elm
@@ -12,10 +12,23 @@ main =
         }
 
 
-type alias Model =
+type alias StringCollection =
     { head : Maybe String
     , tail : Maybe (List String)
     , last : Maybe String
+    }
+
+
+type alias NumberCollection =
+    { head : Maybe Int
+    , tail : Maybe (List Int)
+    , last : Maybe Int
+    }
+
+
+type alias Model =
+    { colors : StringCollection
+    , numbers : NumberCollection
     }
 
 
@@ -30,19 +43,23 @@ colors =
     ]
 
 
-getOne : List String -> Maybe String
-getOne strings =
-    List.head strings
+numbers =
+    [ 0, 1, 2, 3, 4, 5, 6 ]
 
 
-getTail : List String -> Maybe (List String)
-getTail strings =
-    List.tail strings
+getOne : List a -> Maybe a
+getOne list =
+    List.head list
 
 
-getLast : List String -> Maybe String
-getLast strings =
-    case strings of
+getTail : List a -> Maybe (List a)
+getTail list =
+    List.tail list
+
+
+getLast : List a -> Maybe a
+getLast list =
+    case list of
         [] ->
             Nothing
 
@@ -54,9 +71,16 @@ getLast strings =
 
 
 init =
-    { head = getOne colors
-    , tail = getTail colors
-    , last = getLast colors
+    { colors =
+        { head = getOne colors
+        , tail = getTail colors
+        , last = getLast colors
+        }
+    , numbers =
+        { head = getOne numbers
+        , tail = getTail numbers
+        , last = getLast numbers
+        }
     }
 
 
@@ -79,9 +103,48 @@ renderMaybeString maybe =
     div [] nodes
 
 
+renderMaybeInt : Maybe Int -> Html msg
+renderMaybeInt maybe =
+    let
+        nodes =
+            case maybe of
+                Nothing ->
+                    []
+
+                Just int ->
+                    [ text (String.fromInt int) ]
+    in
+    div [] nodes
+
+
+renderCollectionHead : Maybe String -> Html msg
+renderCollectionHead head =
+    renderMaybeString head
+
+
+renderNumberCollectionHead : Maybe Int -> Html msg
+renderNumberCollectionHead head =
+    renderMaybeInt head
+
+
+renderCollectionLast : Maybe String -> Html msg
+renderCollectionLast last =
+    renderMaybeString last
+
+
+renderNumberCollectionLast : Maybe Int -> Html msg
+renderNumberCollectionLast last =
+    renderMaybeInt last
+
+
 renderListItem : String -> Html msg
 renderListItem string =
     li [] [ text string ]
+
+
+renderIntListItem : Int -> Html msg
+renderIntListItem int =
+    li [] [ text (String.fromInt int) ]
 
 
 renderTail : Maybe (List String) -> Html msg
@@ -94,10 +157,47 @@ renderTail list =
             ul [] (List.map renderListItem strings)
 
 
+renderNumberTail : Maybe (List Int) -> Html msg
+renderNumberTail list =
+    case list of
+        Nothing ->
+            ul [] []
+
+        Just ints ->
+            ul [] (List.map renderIntListItem ints)
+
+
+renderCollectionTail : Maybe (List String) -> Html msg
+renderCollectionTail tail =
+    renderTail tail
+
+
+renderNuberCollectionTail : Maybe (List Int) -> Html msg
+renderNuberCollectionTail tail =
+    renderNumberTail tail
+
+
+renderCollection : StringCollection -> Html msg
+renderCollection collection =
+    div []
+        [ renderCollectionHead collection.head
+        , renderCollectionTail collection.tail
+        , renderCollectionLast collection.last
+        ]
+
+
+renderNumberCollection : NumberCollection -> Html msg
+renderNumberCollection collection =
+    div []
+        [ renderNumberCollectionHead collection.head
+        , renderNuberCollectionTail collection.tail
+        , renderNumberCollectionLast collection.last
+        ]
+
+
 view : Model -> Html msg
 view model =
     div []
-        [ renderMaybeString model.head
-        , renderTail model.tail
-        , renderMaybeString model.last
+        [ renderCollection model.colors
+        , renderNumberCollection model.numbers
         ]
